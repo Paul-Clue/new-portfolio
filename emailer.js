d4cab849c0aa9edbd64d95632037c54641d5eec8
Paul-Clue <Paul-Clue@users.noreply.github.com>
2024-07-24 17:25:58 +0000

    deploy: e0378ee2bad25211682b24473295746c9285c95e

--- emailer.js
+++ emailer.js
@@ -2,8 +2,7 @@
 
 // Initialize EmailJS
 // emailjs.init("%%PUBLIC_KEY%%");
-const key = "%%PUBLIC_KEY%%";
-console.log(key);
+
 emailjs.init({
   publicKey: '%%PASSWORD%%',
   // Do not allow headless browsers
@@ -26,8 +25,8 @@ function sendEmail(event) {
   event.preventDefault();
 
   // Collect the form data
-  const serviceID = '%%SERVICE_ID%%';
-  const templateID = '%%TEMPLATE_ID%%';
+  const serviceID = 'service_8z31w8q';
+  const templateID = 'template_lhr2k1a';
 
   const templateParams = {
     name: document.getElementById('name').value,
