6daba77a6590a881546775cd003581da45077748
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-07-23 22:07:56 -0500

    Fix emailer

--- emailer.js
+++ emailer.js
@@ -1,30 +1,48 @@
 // app.js
 
 // Initialize EmailJS
-emailjs.init("%%PUBLIC_KEY%%");
+// emailjs.init("%%PUBLIC_KEY%%");
+emailjs.init({
+  publicKey: '%%PUBLIC_KEY%%',
+  // Do not allow headless browsers
+  blockHeadless: true,
+  blockList: {
+    // Block the suspended emails
+    list: ['foo@emailjs.com', 'bar@emailjs.com'],
+    // The variable contains the email address
+    watchVariable: 'userEmail',
+  },
+  limitRate: {
+    // Set the limit rate for the application
+    id: 'app',
+    // Allow 1 request per 10s
+    throttle: 5000,
+  },
+});
 
 function sendEmail(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Collect the form data
-    const serviceID = '%%SERVICE_ID%%';
-    const templateID = '%%TEMPLATE_ID%%';
+  // Collect the form data
+  const serviceID = '%%SERVICE_ID%%';
+  const templateID = '%%TEMPLATE_ID%%';
 
-    const templateParams = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value,
-    };
+  const templateParams = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
+  };
 
-    emailjs.send(serviceID, templateID, templateParams)
-        .then(response => {
-            console.log('Email sent successfully!', response.status, response.text);
-            alert('Email sent successfully!');
-        })
-        .catch(error => {
-            console.error('Failed to send email.', error);
-            alert('Failed to send email.');
-        });
+  emailjs
+    .send(serviceID, templateID, templateParams)
+    .then((response) => {
+      console.log('Email sent successfully!', response.status, response.text);
+      alert('Email sent successfully!');
+    })
+    .catch((error) => {
+      console.error('Failed to send email.', error);
+      alert('Failed to send email.');
+    });
 }
 
 // Add an event listener to the form
